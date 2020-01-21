36541997ce350548b5b4dd7e17e8d2d60edcab9d
ahewit <ahewit@essex.ac.uk>
2020-01-21 02:06:10 +0000

    Rebuild

--- houseofhewitt/build/service-worker.js
+++ houseofhewitt/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d2c47ac0a3cb62f44203eb86e973f68d.js"
+  "./precache-manifest.e8b10b2a96932d12ca4fc2fa55731b5d.js"
 );
 
 self.addEventListener('message', (event) => {
